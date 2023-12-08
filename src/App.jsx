a39ff527b68f201ab931ed2cbefe0ae3b856d487
André Marques <andre.m.d.abreu@gmail.com>
2023-12-08 09:52:49 -0300

    Importação das páginas + rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Container from "./components/Container";
+import Home from "./pages/Home";
+import ClassicRock from "./pages/ClassicRock";
+import HeavyMetal from "./pages/HeavyMetal";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Container></Container>
+      <Container>
+        <Routes>
+          <Route Component={Home} exact path="/" />
+          <Route Component={ClassicRock} path="/classic-rock" />
+          <Route Component={HeavyMetal} path="/heavy-metal" />
+        </Routes>
+      </Container>
     </BrowserRouter>
   );
 }
