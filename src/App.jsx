6c0c86e174e42ec9c568e4b280c383059be38072
André Marques <andre.m.d.abreu@gmail.com>
2023-12-08 09:25:09 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
+import Container from "./components/Container";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
+      <Container></Container>
     </BrowserRouter>
   );
 }
