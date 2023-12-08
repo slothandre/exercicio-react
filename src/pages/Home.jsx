1b882e6c3d0d01e539043f40d3e80ff8f5fe41a4
André Marques <andre.m.d.abreu@gmail.com>
2023-12-08 09:33:05 -0300

    Criação da página Home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+function Home() {
+  return (
+    <StyledSection>
+      <h2>Home</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In commodi
+        excepturi aut modi hic quidem aspernatur quae placet deserunt explicabo
+        provident offciis, ut labore incidunt ratione magnam maxime soluta
+        volupate.
+      </p>
+    </StyledSection>
+  );
+}
+
+const StyledSection = styled.section`
+  margin: 20px;
+
+  h2 {
+    margin-bottom: 30px;
+  }
+`;
+
+export default Home;
