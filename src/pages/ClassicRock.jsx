573541e09cd30b381d00d6904d8a66787956ac04
André Marques <andre.m.d.abreu@gmail.com>
2023-12-08 09:48:32 -0300

    Create ClassicRock.jsx

---
+++ src/pages/ClassicRock.jsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+function ClassicRock() {
+  return (
+    <StyledSection>
+      <h2>Classic Rock</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore,
+        debitis mollita? Omnis ipsum blanditiis, nam dolores voluptatem at
+        voluptate accusantium.
+      </p>
+      <ul>
+        <li>Black Sabbath</li>
+        <li>Deep Purple</li>
+        <li>Led Zeppelin</li>
+      </ul>
+    </StyledSection>
+  );
+}
+
+const StyledSection = styled.section`
+  margin: 20px;
+
+  h2 {
+    margin-bottom: 30px;
+  }
+`;
+
+export default ClassicRock;
