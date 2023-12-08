ea3b71329060a2df7c9ef6d0d1a68261452dd86b
André Marques <andre.m.d.abreu@gmail.com>
2023-12-08 09:24:53 -0300

    Criação do Container

---
+++ src/components/Container.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+function Container({ children }) {
+  return (
+    <StyledContainer>
+      <h2>blabla</h2>
+      {children}
+    </StyledContainer>
+  );
+}
+
+const StyledContainer = styled.main`
+  width: 90%;
+  margin: 1rem auto;
+  margin-top: 0;
+  background-color: #fff;
+  border: 5px solid #000;
+`;
+
+export default Container;
